9104222b06a396e57fec08b254ec77e5cd1a5e4a
Takuya Kataiwa <takuyakataiwa@icloud.com>
2023-11-14 09:30:32 +0900

    [bug fix]changed little type

--- status/Status.java
+++ status/Status.java
@@ -3,17 +3,49 @@ public class Status {
     private Level level;
     private HP hp;
     private AttackPoint attackPoint;
-    private DefencePoint defensePoint;
+    private DefencePoint defencePoint;
+
+    public Name getName() {
+        return name;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public HP getHp() {
+        return hp;
+    }
+
+    public AttackPoint getAttackPoint() {
+        return attackPoint;
+    }
+
+    public DefencePoint getDefencePoint() {
+        return defencePoint;
+    }
+
+    @Override
+    public String toString() {
+        return "Status{" +
+               "name=" + name +
+               ", level=" + level +
+               ", hp=" + hp +
+               ", attackPoint=" + attackPoint +
+               ", defencePoint=" + defencePoint +
+               '}';
+    }
+
 
     public Status(Name name, Level level, HP hp, AttackPoint attackPoint, DefencePoint defencePoint) {
         this.name = name;
         this.level = level;
         this.hp = hp;
         this.attackPoint = attackPoint;
-        this.defensePoint = defencePoint;
+        this.defencePoint = defencePoint;
     }
 
-    private void loseHP(int value) {
+    public void loseHP(int value) {
         this.hp = this.hp.lose(value);
     }
 }
